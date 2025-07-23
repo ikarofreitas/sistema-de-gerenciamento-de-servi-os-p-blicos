fd346873e5c90cbb74dddc9a4c4e263ee0331d1a
ikruuu <freitasikaro73@gmail.com>
2025-07-23 15:28:06 -0300

    :art: Refatora estrutura de pastas

---
+++ front/src/pages/Organizations.jsx
@@ -0,0 +1,9 @@
+import CardOrganization from "../components/CardOrganization"
+
+export default function Organizations(){
+    return(
+        <div>
+            <CardOrganization />
+        </div>
+    )
+} 
---
+++ front/src/pages/Services.jsx
@@ -0,0 +1,12 @@
+import AddServiceButton from "../components/AddServiceButton"
+import DeleteServiceButton from "../components/DeleteServiceButton"
+import ListServiceButton from "../components/ListServiceButton"
+import CardService from "../components/CardService"
+
+export default function Services(){
+    return(
+        <div>
+        <CardService></CardService>
+        </div>
+    )
+} 
